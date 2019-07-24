fe7a920d4e6e065fe3277ca91e0a00767d971300
Dmitry <dilix90@gmail.com>
2019-07-24 18:48:37 +0300

    Android plugin impl that run at least

--- src/android/SonectCordovaPlugin.java
+++ src/android/SonectCordovaPlugin.java
@@ -1,47 +1,21 @@
-package ch.sonect.sdk.sample
+package ch.sonect.sdk;
 
-import android.content.Intent
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import ch.sonect.sdk.SonectSDK
-import ch.sonect.sdk.paymentPlugins.PaymentConfig
-import ch.sonect.sdk.paymentPlugins.PaymentPlugin
+import android.util.Log;
 
-class SdkWrapperActivity : AppCompatActivity() {
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaArgs;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONException;
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_wrapper)
-
-
-        val builder: SonectSDK.Config.Builder = SonectSDK.Config.Builder()
-        val sonectSDK = SonectSDK(
-            this,
-            builder.enablePaymentPlugin(PaymentPlugin.ADYEN)
-                .enablePaymentPlugin(PaymentPlugin.HBL, PaymentConfig(null, "hbl"))
-                .enablePaymentPlugin(PaymentPlugin.NEON, PaymentConfig(null, "neon"))
-                .enablePaymentPlugin(PaymentPlugin.POSTFINANCE, PaymentConfig(null, "postfinance"))
-                .enablePaymentPlugin(PaymentPlugin.SOFORT, PaymentConfig(null, "sofort"))
-                .enablePaymentPlugin(PaymentPlugin.BALANCE)
-                .build()
-        )
-
-        //Old user
-        //0041785544213
-        //eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRlIjoiMjAxOS0wNC0wNFQxMDoxNjowNC45MDRaIiwicGhvbmUiOiIwMDQxNzg1NTQ0MjEzIiwic2NvcGUiOiJsb2dpbiIsInR5cGUiOiJ1c2VyIiwiaWF0IjoxNTU0MzcyOTY0fQ.gspRt3jfSdjx7TsDb4G8cUvZLJgtY8d5RQ3JwoLFPJg
-
-        // New user
-        //0041786655213
-        //eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRlIjoiMjAxOS0wNC0xMFQxMDowMjo1OS4zODZaIiwicGhvbmUiOiIwMDQxNzg2NjU1MjEzIiwic2NvcGUiOiJsb2dpbiIsInR5cGUiOiJ1c2VyIiwiaWF0IjoxNTU0ODkwNTc5fQ.NWUzxx2Dw0WwhpSm3mhYFfBdkFYvfu74jCGJzpOLGmQ
-
-        supportFragmentManager.beginTransaction().replace(R.id.container, sonectSDK.getStartFragment()).commit()
+public class SonectCordovaPlugin extends CordovaPlugin {
+    @Override
+    public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException {
+        Log.e("!@#", action);
+        Log.e("!@#", args.toString());
+//        Context applicationContext = cordova.getActivity().getApplicationContext();
+//        SDKEntryPointActivity.Companion.start(applicationContext, );
+        return true;
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
 
-        for (fragment in supportFragmentManager.fragments) {
-            fragment.onActivityResult(requestCode, resultCode, data)
-        }
-    }
 }
